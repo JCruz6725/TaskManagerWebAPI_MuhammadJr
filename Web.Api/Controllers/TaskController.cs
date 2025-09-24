a4f93c2c1ae08dde1052b194b8f97c2183924b97
MuhammadJr <Muhammadou1drammeh@gmail.com>
2025-09-24 16:12:38 -0500

    added comments

--- Web.Api/Controllers/TaskController.cs
+++ Web.Api/Controllers/TaskController.cs
@@ -22,13 +22,13 @@ namespace Web.Api.Controllers
         [HttpGet("{taskId}", Name = "GetTaskById")]
         public async Task<ActionResult<TaskDto>> GetTaskById(Guid taskId)
         {
-            var getTasks = await _unitOfWork.TaskItem.GetTaskByIdAsync(taskId);
-            if(getTasks == null)
+            var getTasks = await _unitOfWork.TaskItem.GetTaskByIdAsync(taskId); //UofW that takes the TaskItem and call the TaskItemRepo GetUserById
+            if (getTasks == null)                                               //check if the taskId  is null then return invalid
             {
                 return NotFound("Id is invalid");
             }
 
-            var taskDetail = new TaskDto()
+            var taskDetail = new TaskDto()                                    //create a new instance of TaskDto and set their properties
             {
                 Id = getTasks.Id,
                 Title = getTasks.Title,
@@ -37,33 +37,35 @@ namespace Web.Api.Controllers
                 CreatedDate = getTasks.CreatedDate,
                 CreatedUserId = getTasks.CreatedUserId,
 
-                Notes = getTasks.TaskItemNotes.Select(note =>  new NoteDto
-                {
-                    Id = note.Id,
-                    TaskItemId = note.TaskItemId,
-                    Note = note.Note,
-                    CreatedDate = note.CreatedDate,
-                    CreatedUser = note.CreatedUserId,
-                } ).ToList(), 
+                Notes = getTasks.TaskItemNotes.Select                            //within the TaskDto create a new List of Notes that grabs TaskItemNotes and set their properties
+                    (note => new NoteDto                                         //create new instance of NoteDto
+                    {
+                        Id = note.Id,
+                        TaskItemId = note.TaskItemId,
+                        Note = note.Note,
+                        CreatedDate = note.CreatedDate,
+                        CreatedUser = note.CreatedUserId,
+                    }).ToList(),                                                 //add notes to the list
 
-                CurrentStatus = getTasks.TaskItemStatusHistories.Select(history => new StatusDto
-                {
-                    Id = history.Status.Id,
-                    Name = history.Status.Name,  
-                    Code = history.Status.Code,
-                }).FirstOrDefault(),
+                CurrentStatus = getTasks.TaskItemStatusHistories.Select           //within the TaskDto create a new list of CurrentStatus that grabs task histories and set their properites
+                     (history => new StatusDto                                     //create new instance of StatusDto
+                     {
+                         Id = history.Status.Id,
+                         Name = history.Status.Name,
+                         Code = history.Status.Code,
+                     }).FirstOrDefault(),
             };
-            return Ok(taskDetail);
+            return Ok(taskDetail);                                            //retun task details
 
         }
 
-        [HttpPost( Name = "CreateTask")]
-        public async Task<ActionResult<TaskDto>> CreateTask([FromHeader] Guid UserId,[FromBody]TaskCreateDto taskCreatedDto)
+        [HttpPost(Name = "CreateTask")]
+        public async Task<ActionResult<TaskDto>> CreateTask([FromHeader] Guid UserId, [FromBody] TaskCreateDto taskCreatedDto)
         {
-                                                                                        //Request DTO
-                                                                                        //create a new instance of TaskItem 
-                                                                                       //calls the TaskItem prop and set the task created dto to its prop
-            var taskCreation = new TaskItem()                                            
+            //Request DTO
+            //create a new instance of TaskItem 
+            //calls the TaskItem prop and set the task created dto to its prop
+            var taskCreation = new TaskItem()
             {
                 Title = taskCreatedDto.Title,
                 DueDate = taskCreatedDto.DueDate ?? DateTime.Now.AddDays(5),
@@ -75,10 +77,10 @@ namespace Web.Api.Controllers
             await _unitOfWork.TaskItem.CreateTaskAsync(taskCreation);              //UofW takes the TaskItem class and calls the CreateTask method from the TaskItemRepo
             await _unitOfWork.SaveChangesAsync();                                  //UofW calls the SaveChanges method
 
-                                                                                   //Response DTO
-                                                                                   //create a new instance of TaskDto
-                                                                                  //calls the TaskDto prop and call the taskCreation and set the prop for user view
-                                                                                  //return the result of the tasks created
+            //Response DTO
+            //create a new instance of TaskDto
+            //calls the TaskDto prop and call the taskCreation and set the prop for user view
+            //return the result of the tasks created
             var creationResult = new TaskDto()
             {
                 Id = taskCreation.Id,
@@ -122,8 +124,5 @@ namespace Web.Api.Controllers
             throw new NotImplementedException();
 
         }
-
-        //private readonly TaskItemRepo _taskRepsitory;
-
     }
 }
