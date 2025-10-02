a199636dd85efa321d0fc41be368c47d7bf67b41
JCruz6725 <john.cruz6725@gmail.com>
2025-10-02 09:32:21 -0500

    align task dto

--- Web.Api/Dto/Response/TaskDto.cs
+++ Web.Api/Dto/Response/TaskDto.cs
@@ -3,12 +3,12 @@
     public class TaskDto
     {
         public Guid Id { get; set; }
-        public string MyProperty { get; set; }
+        public string Title { get; set; }
         public DateTime DueDate { get; set; }
         public int Priority { get; set; }
-        public NoteDto Notes { get; set; }
-        public StatusDto CurrentStatus { get; set; }
+        public List<NoteDto> Notes { get; set; } = [];
+        public StatusDto? CurrentStatus { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime CreatedUser { get; set; }
+        public Guid CreatedUserId { get; set; }
     }
 }
