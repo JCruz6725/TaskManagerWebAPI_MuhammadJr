6937895859bc7f845e400b3d5f14ca7d5a6e71e3
JCruz6725 <john.cruz6725@gmail.com>
2025-11-13 11:41:16 -0600

    Reconfigure to use a director layer and refined the user builder fluent api, can now add orphan task.

---
+++ Web.Api/Util/UserBuilder.cs
@@ -0,0 +1,149 @@
+﻿using Web.Api.Persistence.Models;
+
+namespace Web.Api.Util
+{
+    public class UserBuilder(string email, string first, string last, string pass) { 
+        
+        private User user = new User(){ 
+            CreatedDate = DateTime.Now,
+            Email = email,
+            FirstName = first,
+            LastName = last,
+            Password = pass
+        };
+
+        private List? currentList = null;
+        private List<TaskItem> taskList = [];
+        private TaskItem? currentTaskItem = null;
+
+
+        public UserBuilder AddList(string listname) {
+            List list = new() {
+                CreatedDate = DateTime.Now,
+                CreatedUser = user,
+                Name = listname,
+            };
+
+            user.Lists.Add(list);
+            currentList = list;
+            return this;    
+        }
+
+
+        public UserBuilder AddTask(string taskname, Guid StatusPendingId) {
+            if (currentList == null) 
+            { 
+                throw new Exception("Must add list prior to adding a task item."); 
+            }
+
+            TaskItem taskItem = new() {
+                CreatedDate = DateTime.Now,
+                CreatedUser = user,
+                Title = taskname,
+                TaskItemStatusHistories = [
+                    new TaskItemStatusHistory(){
+                        StatusId = StatusPendingId,
+                        CreatedDate = DateTime.Now,
+                        CreatedUser = user,
+                    }
+                ]
+            };
+
+            currentList.TaskWithinLists.Add(
+                new TaskWithinList() { 
+                    CreatedDate = DateTime.Now,
+                    CreatedUser= user,
+                    TaskItem = taskItem
+                }
+            );
+
+            taskList.Add(taskItem);
+            currentTaskItem = taskItem;
+
+            return this;    
+        }
+
+
+        public UserBuilder AddOrphanTask(string taskname, Guid StatusPendingId) {
+            TaskItem taskItem = new() {
+                CreatedDate = DateTime.Now,
+                CreatedUser = user,
+                Title = taskname,
+                TaskItemStatusHistories = [
+                    new TaskItemStatusHistory(){
+                        StatusId = StatusPendingId,
+                        CreatedDate = DateTime.Now,
+                        CreatedUser = user,
+                    }
+                ]
+            };
+
+            user.TaskWithinLists.Add(
+                new TaskWithinList() { 
+                    CreatedDate = DateTime.Now,
+                    CreatedUser= user,
+                    TaskItem = taskItem
+                }
+            );
+
+            taskList.Add(taskItem);
+            currentTaskItem = taskItem;
+
+            return this;    
+        }
+
+
+        public UserBuilder AddNote(string content) {
+            if (currentTaskItem is null) 
+            {
+                throw new Exception("Must create a task item first.");
+            }
+
+            currentTaskItem.TaskItemNotes.Add(
+                new TaskItemNote() { 
+                    CreatedUser = user,
+                    CreatedDate= DateTime.Now,
+                    Note = content
+                }
+            );
+            return this;
+        }
+
+
+
+        public UserBuilder AddSubTask(string listnameP, string parent, string listnameC, string child) {
+            user.SubTasks.Add(
+                new SubTask() { 
+                    CreatedUser = user,
+                    CreatedDate = DateTime.Now,
+                    TaskItem = user.Lists.Single(l => l.Name == listnameP).TaskWithinLists.Single(ti => ti.TaskItem.Title == parent ).TaskItem,
+                    SubTaskItem = user.Lists.Single(l => l.Name == listnameC).TaskWithinLists.Single(ti => ti.TaskItem.Title == child ).TaskItem,
+                }
+            );
+            return this;
+        }
+        
+
+        public UserBuilder AddSubTask(string parent, string child) {
+            if (currentList == null) 
+            { 
+                throw new Exception("Must add list prior to adding a task item."); 
+            }
+
+            user.SubTasks.Add(
+                new SubTask() { 
+                    CreatedUser = user,
+                    CreatedDate = DateTime.Now,
+                    TaskItem = currentList.TaskWithinLists.Single(ti => ti.TaskItem.Title == parent ).TaskItem,
+                    SubTaskItem = currentList.TaskWithinLists.Single(ti => ti.TaskItem.Title == child ).TaskItem,
+                }
+            );
+            return this;
+        }
+
+
+        public User GetFinalUser() { 
+            return user;    
+        }
+    }
+}
---
+++ Web.Api/Util/UserDirector.cs
@@ -0,0 +1,95 @@
+﻿using Web.Api.Persistence.Models;
+
+namespace Web.Api.Util {
+    public class UserDirector(StatusChange statusChange) {
+        public User MakeAlexFarmerProfile() {
+             return new UserBuilder(email: "AFarmer@email.com", first: "Alex", last: "Farmer", pass: "12345")
+                .AddList("Exercise")
+                    .AddTask("Run", statusChange.PendingId)
+
+                .AddList("Some list from FluentAPI")
+                    .AddTask("FluentTask01", statusChange.PendingId)
+                        .AddNote("SomeContent")
+                        .AddNote("SomeOtherContent")
+
+                    .AddTask("Run", statusChange.PendingId)
+                    .AddTask("Buy Shoes", statusChange.PendingId)
+                    .AddTask("Go to park", statusChange.PendingId)
+                    .AddTask("Walk", statusChange.PendingId)
+                        .AddNote("Take the dogs on the walk")
+
+                    .AddSubTask("Run","Buy Shoes")
+                    .AddSubTask("Run","Go to park")
+                    .AddSubTask("Walk","Go to park")
+                .AddList("Random")
+                .GetFinalUser();
+        }
+
+        public User MakeJessieHopkinsProfile() {
+             return new UserBuilder(email: "JHopkins@email.com", first: "Jessie", last: "Hopkins", pass: "password")
+                .AddOrphanTask("Cook", statusChange.PendingId)
+                    .AddNote("spaghetti")
+                    .AddNote("tacos")
+
+                .AddOrphanTask("Buy ingredients", statusChange.PendingId)
+                .AddOrphanTask("Chop veggies", statusChange.PendingId)
+                .AddOrphanTask("Wash dishes", statusChange.PendingId)
+
+                .AddSubTask("Cook", "Buy ingredients")
+                .AddSubTask("Cook", "Chop veggies")
+                .AddSubTask("Cook", "Wash dishes")
+
+                .GetFinalUser();
+        }
+
+
+        public User MakeAprilRiceProfile() {
+             return new UserBuilder(email: "ARice@email.com", first: "April", last: "Rice", pass: "ARice")
+                .AddList("Work")
+
+                .AddOrphanTask("Clean",statusChange.PendingId)
+                    .AddNote("Bathroom")
+                .AddOrphanTask("Clean kitchen",statusChange.PendingId)
+                .AddOrphanTask("Clean bathroom",statusChange.PendingId)
+
+                .AddSubTask("Clean", "Clean kitchen")
+                .AddSubTask("Clean", "Clean bathroom")
+                
+                .GetFinalUser();
+        }
+
+        
+        public User MakeNikoLogamProfile() {
+             return new UserBuilder(email: "Nlogan@email.com", first: "Niko", last: "Logan", pass: "abc")
+                .AddList("New list")
+                
+                .GetFinalUser();
+        }
+
+
+        public User MakeChuckFinleyProfile() {
+             return new UserBuilder(email: "chuck.finley@email.com", first: "Chuck", last: "Finley", pass: "abc")
+                .AddList("Mustang Project")
+                    .AddTask("Test Drive", statusChange.PendingId)
+                        .AddNote("Destination, the mall. Far enough to satisfy the test drive.")
+                    
+                        .AddTask("Fix radiator", statusChange.PendingId)
+                    .AddTask("Add water", statusChange.PendingId)
+                    .AddTask("Check oil", statusChange.PendingId)
+                    .AddTask("Check tire pressure", statusChange.PendingId)
+                    .AddTask("Replace Spark plugs", statusChange.PendingId)
+                        .AddNote("The plugs were fouled from the last test run.")
+                        .AddNote("Auto parts store has a set for $23.45 ou the door.")
+                        .AddNote("Check the price of new distributor and plug wires while at the auto parts store.")
+
+                    .AddSubTask("Test Drive", "Add water")
+                    .AddSubTask("Test Drive", "Check oil")
+                    .AddSubTask("Test Drive", "Check tire pressure")
+                    .AddSubTask("Test Drive", "Replace Spark plugs")
+                    .AddSubTask("Add water", "Fix radiator")
+                
+                .GetFinalUser();
+        }
+
+    }
+}
