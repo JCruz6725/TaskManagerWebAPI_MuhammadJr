b517d5f5eb0110317034bdba2afb5ab9eea52616
jacieylyn <jacquelinee.trann@yahoo.com>
2025-11-21 10:59:48 -0600

    Implemented try-catch to user endpoints

--- Web.Api/Controllers/UserController.cs
+++ Web.Api/Controllers/UserController.cs
@@ -24,40 +24,57 @@ namespace Web.Api.Controllers
         [HttpPost(Name = "RegisterUser")]                              //Http post request 
         public async Task<ActionResult<Guid>> RegisterUser(RegisterUserDto registerUserDto)     //resgister User method user creation
         {
-            User? user = await _unitOfWork.User.GetUserByEmailAsync(registerUserDto.Email);
-            if(user is not null) { 
-                return BadRequest("Email already in use.");
-            }
-
-
-            //RequestDTO
-            //create a new instance of User thats not existing
-            //call the User props and set the registerDto to its assign props 
-            User newUser = new User                               
+            try
             {
-                FirstName = registerUserDto.FirstName,
-                LastName = registerUserDto.LastName,
-                Email = registerUserDto.Email,
-                Password = registerUserDto.Password,
-                CreatedDate = DateTime.Now,
-            };
+                User? user = await _unitOfWork.User.GetUserByEmailAsync(registerUserDto.Email);
+                if (user is not null)
+                {
+                    return BadRequest("Email already in use.");
+                }
 
-            await _unitOfWork.User.CreateUserAsync(newUser);          //UofW takes the User class and calls the CreateUser method from the UserRepo
-            await _unitOfWork.SaveChangesAsync();                          //UofW calls the SaveChanges method
 
-            return Ok(newUser.Id);                                    //a new Id Guid is return once user is registered
+                //RequestDTO
+                //create a new instance of User thats not existing
+                //call the User props and set the registerDto to its assign props 
+                User newUser = new User
+                {
+                    FirstName = registerUserDto.FirstName,
+                    LastName = registerUserDto.LastName,
+                    Email = registerUserDto.Email,
+                    Password = registerUserDto.Password,
+                    CreatedDate = DateTime.Now,
+                };
+
+                await _unitOfWork.User.CreateUserAsync(newUser);          //UofW takes the User class and calls the CreateUser method from the UserRepo
+                await _unitOfWork.SaveChangesAsync();                          //UofW calls the SaveChanges method
+
+                return Ok(newUser.Id);                                    //a new Id Guid is return once user is registered
+            }
+            catch(Exception ex)
+            {
+                _logger.Error($"Register user process failed: {ex.Message}");  
+                return StatusCode(500);
+            }
         }
 
 
         [HttpPost("login", Name = "Login")]
         public async Task<ActionResult<Guid>> Login(LoginDto userLoginDto)           //login user method creation
         {
-            User? userLogin = await _unitOfWork.User.GetUserByEmailAsync(userLoginDto.Email);   //get user from UofW and user email from UserRepo
-            if(userLogin is null || userLogin.Password != userLoginDto.Password) 
+            try
             {
-                return BadRequest("Invalid email or password.");
+                User? userLogin = await _unitOfWork.User.GetUserByEmailAsync(userLoginDto.Email);   //get user from UofW and user email from UserRepo
+                if(userLogin is null || userLogin.Password != userLoginDto.Password) 
+                {
+                    return BadRequest("Invalid email or password.");
+                }
+                return Ok(userLogin.Id);  // return the registered GUID Id of that user  
+            }  
+            catch (Exception ex)
+            {
+                _logger.Error($"Login user process failed: {ex.Message}");
+                return StatusCode(500);
             }
-            return Ok(userLogin.Id);                                     // return the registered GUID Id of that user
         }
     }
 }
