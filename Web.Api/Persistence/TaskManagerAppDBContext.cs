8b729c6c7f1b8d9bb3d3a1e7064c869d71af007c
jacieylyn <jacquelinee.trann@yahoo.com>
2026-03-17 14:28:26 -0500

    Removed comments

--- Web.Api/Persistence/TaskManagerAppDBContext.cs
+++ Web.Api/Persistence/TaskManagerAppDBContext.cs
@@ -43,11 +43,7 @@ public partial class TaskManagerAppDBContext : DbContext
     public virtual DbSet<TaskWithinList> TaskWithinLists { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-    /*
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TaskManagerApp");
-    */
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Address>(entity =>
